34c7a8921725e10e9e5270fe7f54dc1fe8cfd72b
Ioannis Kyriacou <2462278k@student.gla.ac.uk>
2023-07-28 15:47:08 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 
-function App() {
+const App = () => {
+  const [showContent, setShowContent] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowContent(!showContent);
+  };
+
+  const listOfStrings = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5','Item 2', 'Item 3', 'Item 4', 'Item 5','Item 2', 'Item 3', 'Item 4', 'Item 5'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="left-section">
+        <button onClick={handleButtonClick}>QA Manager Gui</button>
+      </div>
+      {showContent && (
+        <div className="right-section">
+          <div className="object">Object 1</div>
+          <div className="object">Object 2</div>
+          <div className="object">Object 3</div>
+          {listOfStrings.map((string, index) => (
+              <li key={index}>{string}</li>
+            ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
